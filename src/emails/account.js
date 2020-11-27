7871aa4f3c2b96ce62fcf85bdc0db82e8f2c1461
Pierre Kollins <pierre.kollins@infosistemas.com.br>
2020-11-27 11:39:30 -0300

    Init commit

---
+++ src/emails/account.js
@@ -0,0 +1,33 @@
+const sgMail = require('@sendgrid/mail')
+
+sgMail.setApiKey(process.env.SENDGRID_API_KEY)
+
+const sendWelcomeEmail = (email, name) => {
+    sgMail.send({
+        to: email,
+        from: 'pierrepneto@gmail.com',
+        subject: 'Thanks for joining in!',
+        text: `Welcome to the app, ${name}. Let me know how yoy get along with the app.`
+    })
+}
+
+const sendCancelationEmail = (email, name) => {
+    sgMail.send({
+        to: email,
+        from: 'pierrepneto@gmail.com',
+        subject: 'Sorry to see you go!',
+        text: `Goodbye, ${name}. I hope to see you back sometime soon.`
+    })
+}
+
+module.exports = {
+    sendWelcomeEmail,
+    sendCancelationEmail
+}
+
+// sgMail.send({
+//     to: 'pierrepneto@gmail.com',
+//     from: 'pierrepneto@gmail.com',
+//     subject: 'this is my first creation!',
+//     text: 'I hope this one actually get to you'
+// })
